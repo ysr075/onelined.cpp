90860ffe3b97f5ccb440f923ae77af0dbc727d99
ysr075 <74384666+ysr075@users.noreply.github.com>
2021-01-13 20:57:20 +0100

    oneLined.cpp

---
+++ oneLined.cpp
@@ -0,0 +1,11 @@
+#include <iostream> 
+using namespace std;
+
+/* 
+programmed by @ysr075;
+one lined looping program;
+*/
+
+int main() {
+	int num; int loop; int i; do { cout << "\nloop: "; cin >> loop; for (i = 0; i < loop; i++) { cout << "\nnum: "; cin >> num; } } while (true);
+}
